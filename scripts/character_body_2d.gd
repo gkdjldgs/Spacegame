18ff586640c4186516cd24d2bb016fb139232c3d
melon <jermaise@gmail.com>
2025-11-11 14:31:01 -0700

    Movment
    I followed a tutorial to implement basic movement for the character and to also look at the cursor
    I also just modify the camera zoom and speed t o make the planet feel larger?

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,25 +1,17 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+var speed = 30
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var strafe = Vector2(Input.get_axis('left',"right",),
+	Input.get_axis('up','down'))
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if strafe != Vector2.ZERO:
+		velocity = speed * strafe
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x,0,speed)
+		velocity.y = move_toward(velocity.y,0,speed)
+		
 	move_and_slide()
