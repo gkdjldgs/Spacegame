2793695f2c387613c42fa18a0b538f3b46285c1a
melon <jermaise@gmail.com>
2025-11-15 00:29:53 -0700

    Groups and enemy funkiness and ducks?
    The player is added to a group, which allows the bullet to know what its detecting by checking the group the area it hits is in.
    The tutorial has an approach using a gun for the enemys, but I'm using a ufo for it to work so I'll have to take a dif approach. I'm wondering if I can copy the code used for summoning the bullets for the ufo, for the player whichuses signals to send data about the location need to spawn the bullet.
    Also I just wanted to include duck coding, I felt really sad about not talking about it. Theres this one concept the goat jmbiv talks about, where instead of checking for an bodies ttype you check for its attributes
    Basically if it looks like a duck and it acts like a duck and sounds like a duck its probably a duck(but for coding)

--- AI.gd
+++ AI.gd
@@ -1,7 +1,7 @@
 extends Node2D
 @onready var detection = $Area2D/detection
 signal state_changed(new_state)
-@onready var player = $Player
+@onready var player = null
 
 enum State{
 	LOOK,
@@ -10,6 +10,16 @@ enum State{
 
 var current_state: int = State.LOOK: set = set_state
 
+func _process(delta: float) -> void:
+	match current_state:
+		State.LOOK:
+			pass
+		State.ATTACK:
+			if player != null:
+				pass
+		_:
+			pass
+			print('nostateforenemy')
 func set_state(new_state: int):
 	if new_state == current_state:
 		return
@@ -20,4 +30,6 @@ func set_state(new_state: int):
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group('player'):
+		set_state(State.ATTACK)
+		player = body
